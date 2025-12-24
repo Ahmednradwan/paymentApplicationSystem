a7daa1add0b01d3489fdbcffc1b5026db1d1d84f
Ahmed Nour Radwan <ahmed_n_radwan@hotmail.com>
2025-12-24 23:03:32 +0200

    Creating the project

---
+++ src/card.h
@@ -0,0 +1,7 @@
+/* Card Header File */
+/*
+* @author: Ahmed Nour Radwan
+* @date: 24/12/2025
+* @version: 1.0
+* @description: This file contains the declarations related to card operations in the Payment Application System.
+*/
